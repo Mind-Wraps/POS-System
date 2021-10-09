d1e260c564b0ad59e8b8aba5c060439ec7812f97
Sandakelum98 <sandakelumsanjaya@gmail.com>
2021-10-09 10:39:00 +0530

    create customer files

--- src/lk/mindwraps/pos/controller/CustomerController.java
+++ src/lk/mindwraps/pos/controller/CustomerController.java
@@ -1,8 +1,28 @@
 package lk.mindwraps.pos.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
+import javafx.scene.input.InputMethodTextRun;
+import lk.mindwraps.pos.db.DBConnection;
+import lk.mindwraps.pos.model.Customer;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  * @author Sanjaya Sandakelum <sandakelumsanjaya@gmail.com> (CREATIVE-2)
  * @since 2021-10-09
  */
 public class CustomerController {
+    public TextField txtId;
+    public TextField txtName;
+
+    public void saveOnAction(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
+
+
+
+    }
+
 }
