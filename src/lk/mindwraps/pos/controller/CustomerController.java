7305a4dd37561f495e4f403f95b7e1bd0fffa4bc
Sandakelum98 <sandakelumsanjaya@gmail.com>
2021-10-09 10:11:51 +0530

    create develop branch

--- src/lk/mindwraps/pos/controller/CustomerController.java
+++ src/lk/mindwraps/pos/controller/CustomerController.java
@@ -4,5 +4,5 @@ package lk.mindwraps.pos.controller;
  * @author Sanjaya Sandakelum <sandakelumsanjaya@gmail.com> (CREATIVE-2)
  * @since 2021-10-09
  */
-public class Dashboard {
+public class CustomerController {
 }
