21e238b493ce2db878bd85c92b12636aad561070
Sandakelum98 <sandakelumsanjaya@gmail.com>
2021-10-09 11:12:41 +0530

    save item close #2

--- src/lk/mindwraps/pos/controller/ItemController.java
+++ src/lk/mindwraps/pos/controller/ItemController.java
@@ -16,5 +16,29 @@ import java.sql.SQLException;
  * @since 2021-10-09
  */
 public class ItemController {
+    public TextField txtId;
+    public TextField txtName;
 
+    public void itemOnAction(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
+
+        Item item = new Item(
+                txtId.getText(),
+                txtName.getText()
+        );
+
+        PreparedStatement stm = DBConnection.getInstance().
+                getConnection().prepareStatement(
+                "INSERT INTO Item VALUES(?,?,?,?)");
+        stm.setString(1, item.getId());
+        stm.setString(2, item.getName());
+
+        if (stm.executeUpdate() > 0) {
+            new Alert(Alert.AlertType.CONFIRMATION,
+                    "Saved!", ButtonType.OK).show();
+        } else {
+            new Alert(Alert.AlertType.WARNING,
+                    "Try Again!", ButtonType.OK).show();
+        }
+
+    }
 }
