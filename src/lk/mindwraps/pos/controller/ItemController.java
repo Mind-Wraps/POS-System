4b7a007c27accd996789c254bf0f5dc61d8cf418
Sandakelum98 <sandakelumsanjaya@gmail.com>
2021-10-09 11:07:28 +0530

    create file for item

---
+++ src/lk/mindwraps/pos/controller/ItemController.java
@@ -0,0 +1,20 @@
+package lk.mindwraps.pos.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
+import lk.mindwraps.pos.db.DBConnection;
+import lk.mindwraps.pos.model.Customer;
+import lk.mindwraps.pos.model.Item;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * @author Sanjaya Sandakelum <sandakelumsanjaya@gmail.com> (CREATIVE-2)
+ * @since 2021-10-09
+ */
+public class ItemController {
+
+}
